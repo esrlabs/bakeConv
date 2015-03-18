85184755080672739786e417b3d56686da518a7a
Frauke Blossey <frauke.blossey@esrlabs.com>
2015-03-18 08:59:01 +0100

    read bake information from Converter.config and prepare bake call

--- bakeConverter.rb
+++ bakeConverter.rb
@@ -2,9 +2,10 @@
 # Author: Frauke Blossey
 # Start @ 12.03.2015
 
+# includes
+require 'getoptlong'
+  
 # get command line arguments:
-require 'getoptlong'  
-
 #unless ARGV.length == 2
 #  puts 'Usage: ruby bakeToConverter.rb -f bakeToConverter/Converter.config -w C:\_dev\projects'  
 #  exit  
@@ -26,23 +27,47 @@ opts.each do |opt, arg|
   end  
 end 
 
-puts workingdir
-
 # read Converter.config file to get all necessary parameters
-File.open(workingdir+converter_config_file) do |f|
-  f.each_line do |line|
-    mainProj = line.gsub('MainProj')
+#File.open(workingdir+converter_config_file) do |f|
+#  File.readlines(f).each do |line| 
+#  end
+#end
+
+fileName = workingdir + converter_config_file
+
+def wordExistsInFile(fileName, seekingExpression)
+  File.open(fileName) do |f|
+    File.readlines(f).each do |line|
+      line.match(seekingExpression) do
+        ar = line.split(" = ")
+        return false if ar.length != 2
+        return ar
+      end
+    end
   end
 end
 
+mainProj = wordExistsInFile(fileName, "MainProj")
+buildConfig = wordExistsInFile(fileName, "BuildConfig")
+proj2Convert = wordExistsInFile(fileName, "Proj2Convert")
+
+bakeMap = {}
+bakeMap[mainProj[0].strip] = mainProj[1].strip
+bakeMap[buildConfig[0].strip] = buildConfig[1].strip
+bakeMap[proj2Convert[0].strip] = proj2Convert[1].strip
+
 # call bake to get the necessary bakeOutput file
-#def bake( mainProj, buildConfig, proj2Config )
-#	cmd = "bake -m " + mainProj + " -b " + buildConfig + " -p " + proj2Config + " --printConverter"
+def bake( bakeMap )
+	cmd = "bake -m " + bakeMap['MainProj'] + " -b " + bakeMap['BuildConfig'] + " -p " + bakeMap['Proj2Convert'] + " --printConverter"
 #	status = system( cmd )
 #	if status == false
 #		abort 'Error while trying to call bake!'
 #	end
-#end
+    puts cmd
+    return "Files/\BakeOutput.txt"
+end
 
+
+bakeFileName = bake(bakeMap)
 
 
